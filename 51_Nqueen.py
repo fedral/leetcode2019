b20eb58b515522aac4e12c5ef4d9a0e95e221135
Fu Zeshan <fuzeshan2012@163.com>
2019-06-04 09:40:45 +0800

    Update 51_Nqueen.py

--- 51_Nqueen.py
+++ 51_Nqueen.py
@@ -7,6 +7,7 @@ Created on Sun Jun  2 10:56:47 2019
 
 
 class Solution:
+    
     def solveNQueens(self, n):
         def could_place(row, col):
             return not (cols[col] + hill_diagonals[row - col] + dale_diagonals[row + col])
@@ -55,7 +56,27 @@ class Solution:
 
 
 
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        res = []
+        s = "."*n
+        def bt(i,tmp,col,diag1,diag2):
+            if(i==n):
+                res.append(tmp)
+            for j in range(n):
+                if( not j in col and not i+j in diag1 and not i-j in diag2 ):
+                    bt( i+1,tmp+[s[:j]+"Q"+s[j+1:]], col+[j], diag1+[i+j],    )
+        bt(0,[],[],[],[])
+        return res
 
+ '''
+ 执行用时 : 52 ms, 在N-Queens的Python提交中击败了95.54% 的用户
+内存消耗 : 11.7 MB, 在N-Queens的Python提交中击败了40.91% 的用户
+ '''
 
 
 
