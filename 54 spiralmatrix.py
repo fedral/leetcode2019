04879161adec03d2b1097205f900cd45e55b8eb0
Fu Zeshan <fuzeshan2012@163.com>
2019-06-08 15:47:29 +0800

    Update 54 spiralmatrix.py

--- 54 spiralmatrix.py
+++ 54 spiralmatrix.py
@@ -27,3 +27,31 @@ class Solution(object):
 内存消耗 : 11.8 MB, 在Spiral Matrix的Python提交中击败了13.77% 的用户
 
 '''
+
+def spiral_coords(r1, c1, r2, c2):
+            for c in range(c1, c2 + 1):
+                yield r1, c
+            for r in range(r1 + 1, r2 + 1):
+                yield r, c2
+            if r1 < r2 and c1 < c2:
+                for c in range(c2 - 1, c1, -1):
+                    yield r2, c
+                for r in range(r2, r1, -1):
+                    yield r, c1
+
+        if not matrix: return []
+        ans = []
+        r1, r2 = 0, len(matrix) - 1
+        c1, c2 = 0, len(matrix[0]) - 1
+        while r1 <= r2 and c1 <= c2:
+            for r, c in spiral_coords(r1, c1, r2, c2):
+                ans.append(matrix[r][c])
+            r1 += 1; r2 -= 1
+            c1 += 1; c2 -= 1
+        return ans
+
+'''
+
+执行用时 : 16 ms, 在Spiral Matrix的Python提交中击败了98.47% 的用户
+内存消耗 : 11.5 MB, 在Spiral Matrix的Python提交中击败了37.92% 的用户
+'''
