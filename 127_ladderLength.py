16b05d18ca8a77586ec4db122c3283914cda6344
Fu Zeshan <fuzeshan2012@163.com>
2019-06-16 10:48:26 +0800

    Update 127_ladderLength.py
    add faster sloution2

--- 127_ladderLength.py
+++ 127_ladderLength.py
@@ -44,7 +44,39 @@ class Solution(object):
 
 
 
-
+## faster
+def ladderLength(self, beginWord, endWord, wordList):
+    """
+    :type beginWord: str
+    :type endWord: str
+    :type wordList: List[str]
+    :rtype: int
+    """
+    if endWord not in wordList:
+        return 0
+    l = len(endWord)
+    ws = set(wordList)
+    
+    head = {beginWord}
+    tail = {endWord}
+    tmp = list('abcdefghijklmnopqrstuvwxyz')
+    res = 1
+    while head:
+        if len(head) > len(tail):
+            head, tail = tail, head
+        q = set()
+        for cur in head:
+            for i in range(l):
+                for j in tmp:
+                    word = cur[:i] + j + cur[i+1:]
+                    if word in tail:
+                        return res + 1
+                    if word in ws:
+                        q.add(word)
+                        ws.remove(word)
+        head = q
+        res += 1
+    return 0
 
 
 
