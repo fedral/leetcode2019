8844810f1ebb2d49805eafafcb1f183f24885f2c
Fu Zeshan <fuzeshan2012@163.com>
2019-06-16 10:01:33 +0800

    128 72

---
+++ 128_longestConsecutive.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 16 10:00:49 2019
+
+@author: fed
+"""
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max( longest_streak, current_streak )
+
+        return longest_streak
+
+
