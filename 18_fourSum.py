6d9eaf6ea65a089ddd09f7b6f9a7161e9ccf9aa3
Fu Zeshan <fuzeshan2012@163.com>
2019-06-18 11:43:25 +0800

    Create 18_fourSum.py

---
+++ 18_fourSum.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 11:40:18 2019
+
+@author: fed
+"""
+
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        n = len(nums)
+        if n < 4: return []
+        nums.sort()
+        res = []
+        for i in range(n-3):
+            if i > 0 and nums[i] == nums[i-1]:continue
+            if nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target:break
+            if nums[i] + nums[n-1] + nums[n-2] + nums[n-3] < target:continue
+            for j in range(i+1,n-2):
+                if j - i > 1 and nums[j] == nums[j-1]:continue
+                if nums[i] + nums[j] + nums[j+1] + nums[j+2] > target:break
+                if nums[i] + nums[j] + nums[n-1] + nums[n-2] < target:continue
+                
+                # 双指针
+                left = j + 1
+                right = n - 1
+                while left < right:
+                    tmp = nums[i] + nums[j] + nums[left] + nums[right]
+                    if tmp == target:
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        while left < right and nums[left] == nums[left+1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right-1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif tmp > target:
+                        right -= 1
+                    else:
+                        left += 1
+        return res
+
+
+
+
