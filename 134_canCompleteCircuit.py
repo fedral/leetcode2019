321e205a45ef5b6e2161b8d76ba50f072d1d7235
Fu Zeshan <fuzeshan2012@163.com>
2019-06-22 16:22:01 +0800

    Create 134_canCompleteCircuit.py

---
+++ 134_canCompleteCircuit.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 16:21:36 2019
+
+@author: fed
+"""
+
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        curr = 0
+        start = 0
+        sum = 0
+        for i in range(start,start + len(gas)):
+            curr += gas[i%len(gas)]
+            curr -= cost[i%len(gas)]
+            sum += gas[i%len(gas)]-cost[i%len(gas)]
+            if curr < 0:
+                curr = 0
+                start = i+1
+                
+        if curr >= 0 and sum >= 0:
+            return start
+        return -1
+    
+    
