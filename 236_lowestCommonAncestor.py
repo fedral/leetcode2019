b43a725a193262e1494a72acfbb373c73808e190
Fu Zeshan <fuzeshan2012@163.com>
2019-06-23 14:13:10 +0800

    Create 236_lowestCommonAncestor.py

---
+++ 236_lowestCommonAncestor.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 14:12:52 2019
+
+@author: fed
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or root == p or root == q:
+            return root
+        
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r:
+            return root
+        elif l:
+            return l
+        elif r:
+            return r
+        else:
+            return
+        
+        
