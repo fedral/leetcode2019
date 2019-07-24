ca7022e8f663c8e288de2fb1b9ecc6cd6f8438e8
fedral <fuzeshan2012@163.com>
2019-07-24 16:55:16 +0800

    Update 5_longestPalindrome.py

--- 5_longestPalindrome.py
+++ 5_longestPalindrome.py
@@ -1,55 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 15 09:40:52 2019
-
-@author: fed
-"""
-
 class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        size = len(s)
-        if size == 0:
-            return ''
-
-        # 至少就是 1
-        longest_palindrome = 1
-        longest_palindrome_str = s[0]
-
-        for i in range(size):
+        if len(s)==1:
+            return s
+        if s=="":
+            return ""
+        l=len(s)
+        '''
+        对于每个位置，利用中心扩散法 找到最长回文子串，时间复杂度O(N^2)
+        '''
+        def centerspread(left,right):
+            if left>right:
+                return 
+            else:
+                while left>=0 and right<l and s[left]==s[right]:
+                    left-=1
+                    right+=1
+                return s[left+1:right],right-left-1
+                    
+        res=""
+        for i in range(len(s)):
+            s1,l1=centerspread(i,i)
+            s2,l2=centerspread(i,i+1)
+            tmp = s1 if l1>=l2 else s2
+            res = res if len(res)>=len(tmp) else tmp
+        return res
+                
+    
+'''
+执行用时 :696 ms, 在所有 Python 提交中击败了72.31%的用户
+内存消耗 :11.9 MB, 在所有 Python 提交中击败了32.27%的用户
+'''
+             
             
-            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
-            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
-
-            
-            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
-            if len(cur_max_sub) > longest_palindrome:
-                longest_palindrome = len(cur_max_sub)
-                longest_palindrome_str = cur_max_sub
-
-        return longest_palindrome_str
-
-    def __center_spread(self, s, size, left, right):
-        l = left
-        r = right
-        while l >= 0 and r < size and s[l] == s[r]:
-            l -= 1
-            r += 1
-        return s[l + 1:r], r - l - 1
+# to-do  马拉车算法  O(N)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        
+        
